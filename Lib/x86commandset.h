6d5fac6554cecc2ab5e8441037b8d4c4976ea5d4
apselon <ildarovadam@gmail.com>
2020-05-23 19:29:14 +0300

    [5]: Added more instructions

--- Lib/x86commandset.h
+++ Lib/x86commandset.h
@@ -39,6 +39,9 @@ namespace Assembly {
 		virtual const char* elf();
 		virtual size_t size();
 	};
+//===========================================================================//
+//                                   MOV
+//===========================================================================//
 
 	class MovReg2Reg: public Instruction {
 		private:
@@ -98,6 +101,37 @@ namespace Assembly {
 		}
 
 	};
+	
+	class MovReg2Mem: public Instruction {
+	private:
+		Registers::Reg src_reg = Registers::NOT_REG;
+		Registers::Reg dst;
+		int64_t offset = 0;
+
+	public:
+		MovReg2Mem(Registers::Reg dst, int64_t offset, Registers::Reg src_reg): src_reg(src_reg), dst(dst), offset(offset) {}
+		MovReg2Mem(int64_t offset, Registers::Reg src_reg): src_reg(src_reg), offset(offset) {}
+
+		const char* assembly(){
+
+			static char output[128] = "";
+
+			if (src_reg != Registers::NOT_REG){
+				sprintf(output, "\tmov [%s + %ld], %s", Registers::names[dst], offset, Registers::names[src_reg]);
+			}
+
+			else {
+				sprintf(output, "\tmov [%ld], %s", offset, Registers::names[src_reg]);
+			}
+
+			return output;
+		}
+
+	};
+
+//===========================================================================//
+//                                   ADD
+//===========================================================================//
 
 	class AddReg2Reg: public Instruction {
 	private:
@@ -115,13 +149,45 @@ namespace Assembly {
 		}
 	};
 
+	class AddMem2Reg: public Instruction {
+	private:
+		Registers::Reg src_reg = Registers::NOT_REG;
+		Registers::Reg dst;
+		int64_t offset = 0;
+
+	public:
+		AddMem2Reg(Registers::Reg dst, Registers::Reg src_reg, int64_t offset): src_reg(src_reg), dst(dst), offset(offset) {}
+		AddMem2Reg(Registers::Reg dst, int64_t offset): dst(dst), offset(offset) {}
+
+		const char* assembly(){
+
+			static char output[128] = "";
+
+			if (src_reg != Registers::NOT_REG){
+				sprintf(output, "\tadd %s, [%s + %ld]", Registers::names[dst], Registers::names[src_reg], offset);
+			}
+
+			else {
+				sprintf(output, "\tadd %s, [%ld]", Registers::names[dst], offset);
+			}
+
+			return output;
+		}
+
+	};
+
+//===========================================================================//
+//                                    SUB
+//===========================================================================//
+
 	class SubReg2Reg: public Instruction {
 	private:
 		Registers::Reg dst;
 		Registers::Reg src;
 
-		SubReg2Reg(Registers::Reg dst, Registers::Reg src): dst(dst), src(src) {};
 	public:	
+		SubReg2Reg(Registers::Reg dst, Registers::Reg src): dst(dst), src(src) {};
+
 		const char* assembly(){
 
 			static char output[128] = "";
@@ -131,13 +197,18 @@ namespace Assembly {
 		}
 	};
 
+//===========================================================================//
+//                                   MUL
+//===========================================================================//
+
 	class MulReg2Reg: public Instruction {
 	private:
 		Registers::Reg dst;
 		Registers::Reg src;
 
-		MulReg2Reg(Registers::Reg dst, Registers::Reg src): dst(dst), src(src) {};
 	public:	
+		MulReg2Reg(Registers::Reg dst, Registers::Reg src): dst(dst), src(src) {};
+
 		const char* assembly(){
 
 			static char output[128] = "";
@@ -147,13 +218,18 @@ namespace Assembly {
 		}
 	};
 
+//===========================================================================//
+//                                  DIV
+//===========================================================================//
+
 	class DivReg2Reg: public Instruction {
 	private:
 		Registers::Reg dst;
 		Registers::Reg src;
 
-		DivReg2Reg(Registers::Reg dst, Registers::Reg src): dst(dst), src(src) {};
 	public:	
+		DivReg2Reg(Registers::Reg dst, Registers::Reg src): dst(dst), src(src) {};
+
 		const char* assembly(){
 
 			static char output[128] = "";
@@ -162,4 +238,147 @@ namespace Assembly {
 			return output;
 		}
 	};
+
+//===========================================================================//
+//                                  RET
+//===========================================================================//
+
+	class Ret: public Instruction {
+	public:	
+		const char* assembly(){
+			static char output[128] = "\t ret";
+			return output;
+		}
+	};
+
+//===========================================================================//
+//                                  JMP
+//===========================================================================//
+//
+	class Jmp: public Instruction {
+	private:
+		int64_t offset = 0;
+		const char* label;
+
+	public:	
+		Jmp(const char* label): label(label) {};
+
+		const char* assembly(){
+			static char output[128] = "";
+			sprintf(output, "\tjmp %s", label); 
+
+			return output;
+		}
+	};
+
+	class Jz: public Instruction {
+	private:
+		int64_t offset = 0;
+		const char* label;
+
+	public:	
+		Jz(const char* label): label(label) {};
+
+		const char* assembly(){
+			static char output[128] = "";
+			sprintf(output, "\tjz %s", label); 
+
+			return output;
+		}
+	};
+
+	class Jnz: public Instruction {
+	private:
+		int64_t offset = 0;
+		const char* label;
+
+	public:	
+		Jnz(const char* label): label(label) {};
+
+		const char* assembly(){
+			static char output[128] = "";
+			sprintf(output, "\tjnz %s", label); 
+
+			return output;
+		}
+	};
+
+	class Jg: public Instruction {
+	private:
+		int64_t offset = 0;
+		const char* label;
+
+	public:	
+		Jg(const char* label): label(label) {};
+
+		const char* assembly(){
+			static char output[128] = "";
+			sprintf(output, "\tjg %s", label); 
+
+			return output;
+		}
+	};
+
+	class Jge: public Instruction {
+	private:
+		int64_t offset = 0;
+		const char* label;
+
+	public:	
+		Jge(const char* label): label(label) {};
+
+		const char* assembly(){
+			static char output[128] = "";
+			sprintf(output, "\tjge %s", label); 
+
+			return output;
+		}
+	};
+
+	class Jl: public Instruction {
+	private:
+		int64_t offset = 0;
+		const char* label;
+
+	public:	
+		Jl(const char* label): label(label) {};
+
+		const char* assembly(){
+			static char output[128] = "";
+			sprintf(output, "\tjl %s", label); 
+
+			return output;
+		}
+	};
+
+	class Jle: public Instruction {
+	private:
+		int64_t offset = 0;
+		const char* label;
+
+	public:	
+		Jle(const char* label): label(label) {};
+
+		const char* assembly(){
+			static char output[128] = "";
+			sprintf(output, "\tjle %s", label); 
+
+			return output;
+		}
+	};
+
+	class CmpReg2Reg: public Instruction {
+	private:
+		Registers::Reg dst;
+		Registers::Reg src;
+	
+	public:
+		CmpReg2Reg(Registers::Reg dst, Registers::Reg src): dst(dst), src(src){};
+		const char* assembly(){
+			static char output[128] = "";
+			sprintf(output, "\tcmp %s, %s", Registers::names[dst], Registers::names[src]); 
+			return output;
+		}
+	};
+
 }
