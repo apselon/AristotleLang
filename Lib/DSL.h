b6e04da71499c5f64ba093f100ae505e53c1973c
apselon <ildarovadam@gmail.com>
2020-05-23 10:13:18 +0300

    [4]: Added DSL elements

--- Lib/DSL.h
+++ Lib/DSL.h
@@ -1,2 +1,3 @@
 #define SPEC_CONNECTION TokenizerNS::Token("CONNECTION", TokenizerNS::SPEC, Operator::CONNECT)
 #define SPEC_BLOCK TokenizerNS::Token("BLOCK", TokenizerNS::SPEC, Operator::BLOCK)
+#define SPEC_OP TokenizerNS::Token("OP", TokenizerNS::SPEC, Operator::BLOCK)
